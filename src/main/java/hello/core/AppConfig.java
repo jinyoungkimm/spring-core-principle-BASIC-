ab638ab99673e46fd2790c09d062e3c77a021960
Jin Young Kim <wlsdud66523@kakao.com>
2025-12-07 21:19:57 +0900

    セクション4(OOP適用)ーAppConfigのリファクタリング

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
@@ -10,21 +11,36 @@ import hello.core.order.OrderServiceImpl;
 
 
 /**
- * 関心の分離(Soc : Separation Of Concerns)
- * →　各クラスの依存関係の生成・注入はAppConfigクラスが担当
- * →　それによりMemberServiceImpl, OrderServiceImpl等のクライアント側はサーバー側を気にせず、各クラス本来の本来の機能実装だけ気にすればよし
- * → 　例えば、MemberServiceImplクラスはMemberRepositoryクラスがメモリーDBであろうがRDBMSだろうが全然気にしなくてよし
+ * ２点の問題点
+ * ・アプリケーションの全体構成が把握しにくい
+ * →　役割　: メソッドのリターンタイプとメソッド名で把握
+ * →　実装　: メソッド内の "return new ~~"部分で把握
+ * →　上記の箇所で役割と実装が明白に把握できるようにする
+ * 
+ * ・new MemoryMemberRepository()という重複が存在
+ * →　容易な変更のため、メソッド化
  */
+
 public class AppConfig {
 
-    public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+    public MemberService memberService() { // 役割
+        return new MemberServiceImpl(memberRepository()); // 実装
     }
 
-    public OrderService orderService() {
-        return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new RateDiscountPolicy()
+    public OrderService orderService() {// 役割
+        return new OrderServiceImpl(// 実装
+                memberRepository(),
+                discountPolicy()
         );
     }
+
+    private DiscountPolicy discountPolicy() {// 役割
+        return new RateDiscountPolicy();// 実装
+    }
+
+    private MemberRepository memberRepository() {// 役割
+        return new MemoryMemberRepository();// 実装
+    }
+
+
 }
