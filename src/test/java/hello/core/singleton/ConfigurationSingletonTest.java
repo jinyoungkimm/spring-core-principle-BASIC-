2d89bf13d02f520036bdd7753ed85ee98faf78e1
Jin Young Kim <wlsdud66523@kakao.com>
2025-12-14 10:16:10 +0900

    セクション6ー@Configurationとバイトコード操作の魔法

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -27,4 +27,17 @@ class ConfigurationSingletonTest {
         // memberRepository1 == memberRepository2
         Assertions.assertThat(memberRepository1).isSameAs(memberRepository2);
     }
+
+    @Test
+    void configurationDeep() {
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig app = ac.getBean(AppConfig.class);
+
+        System.out.println("AppConfigクラス情報 : " + app.getClass());
+        // 出力結果 : AppConfigクラス情報 : class hello.core.AppConfig$$SpringCGLIB$$0
+        // → AppConfig名の後ろに【SpringCGLIB】というのが付いている。我々が登録したのはAppConfigであってAppConfig$$SpringCGLIBではない。
+        // 一体何が起きたのだろうか。結論から言うとSpringは＠Configurationが付いてクラスCGLIBというバイトコード操作ライブラリを使って継承して
+        // シングルトンを担保する(詳細はmemo参照)
+    }
 }
