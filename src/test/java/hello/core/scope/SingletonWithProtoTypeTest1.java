53c3fb33b32f4744ec1d30662441336f4c02cbc3
Jin Young Kim <wlsdud66523@kakao.com>
2026-01-12 05:09:40 +0900

    セクション１０ - プロトタイプスコープ - シングルトンBeanと併用時 プロバイダーで問題解決(Part1)

--- src/test/java/hello/core/scope/SingletonWithProtoTypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithProtoTypeTest1.java
@@ -3,7 +3,9 @@ package hello.core.scope;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -90,17 +92,17 @@ class SingletonWithProtoTypeTest1 {
      */
 
     @Scope("singleton")
-    static class ClientBeanTemp {
+    static class ClientBeanTemp1 {
 
-        private final AnnotationConfigApplicationContext ac; // DIP違反
+        private final ApplicationContext ac; // 訂正) Spring Containerへの依存問題発生　→　純粋なJAVAテストコード作成不可能等
 
-        ClientBeanTemp(AnnotationConfigApplicationContext ac) {
+        ClientBeanTemp1(ApplicationContext ac) {
             this.ac = ac;
         }
 
         public int logic() {
-            ProtoTypeBean protoTypeBean = ac.getBean(ProtoTypeBean.class); // logic()が呼び出される度に新しいprotoTypeBeanオブジェクト生成。
-            protoTypeBean.addCount();
+            ProtoTypeBean protoTypeBean = ac.getBean(ProtoTypeBean.class); // Dependency LookUp(DL) : このように外部(Spring Containerなど)から自動的に外部から
+            protoTypeBean.addCount();                                      // 依存関係を注入してもらうのではなく、開発者が必要な依存関係注を直接探して注入すること。
             int count = protoTypeBean.getCount();
             return count;
         }
@@ -109,13 +111,57 @@ class SingletonWithProtoTypeTest1 {
     @Test
     void singletonWithProtoType2() {
 
-        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBeanTemp.class, ProtoTypeBean.class);
-        ClientBeanTemp bean1 = ac.getBean(ClientBeanTemp.class);
+        ApplicationContext ac = new AnnotationConfigApplicationContext(ClientBeanTemp1.class, ProtoTypeBean.class);
+        ClientBeanTemp1 bean1 = ac.getBean(ClientBeanTemp1.class);
         int count1 = bean1.logic(); // 新しいprotoTypeBean生成
         assertThat(count1).isEqualTo(1);
 
-        ClientBeanTemp bean2 = ac.getBean(ClientBeanTemp.class);
+        ClientBeanTemp1 bean2 = ac.getBean(ClientBeanTemp1.class);
         int count2 = bean2.logic(); // また新しいprotoTypeBean生成
         assertThat(count2).isEqualTo(1);
     }
+
+    /**
+     * ObjectProvider, ObjectFactory : DL機能を提供するSpring API
+     * →指定した依存関係のBeanを開発者がSpring Containerで直接探すのではく、代わりSpring Containerから探して注入する
+     * ※今回の場合はprotoType Beanを注入するから「探す」という表現ようりはSpring Containerに新しいprotoType Beanの生成を依頼して注入するという理解が正しい。
+     */
+
+    @Scope("singleton")
+    static class ClientBeanTemp2 {
+
+        private final ObjectProvider<ProtoTypeBean> protoTypeBeanProvier;
+
+        @Autowired
+        ClientBeanTemp2(ObjectProvider<ProtoTypeBean> protoTypeBeanProvier) {
+            this.protoTypeBeanProvier = protoTypeBeanProvier; // ObjectProviderがSpring ContainerでBeanを自動的に探して注入する
+        }
+
+        public int logic() {
+            ProtoTypeBean protoTypeBean = protoTypeBeanProvier.getObject(); // 依存関係注入
+            protoTypeBean.addCount();
+            int count = protoTypeBean.getCount();
+            return count;
+        }
+    }
+
+    @Test
+    void singletonWithProtoType3() {
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(ClientBeanTemp2.class, ProtoTypeBean.class);
+        ClientBeanTemp2 bean1 = ac.getBean(ClientBeanTemp2.class);
+        int count1 = bean1.logic(); // 新しいprotoTypeBean生成
+        assertThat(count1).isEqualTo(1);
+
+        ClientBeanTemp2 bean2 = ac.getBean(ClientBeanTemp2.class);
+        int count2 = bean2.logic(); // また新しいprotoTypeBean生成
+        assertThat(count2).isEqualTo(1);
+    }
+
+    /**
+     * DL機能を使うより、相変わらずSpringへ依存はしているが(ObjectProvider, ObjectFactory), Spring Container機能を最小限だけ使うので
+     * 以前と比べて相対的にテストコードの作成がしやすくなった。
+     * →しかし、Springへの依存は残っているのでテストコード作成制限などの制約が残ってはいる。
+     * このために誕生したのが次回学習するjakarta.inject.ProviderというJSR-330 JAVA標準 APIを使うことである。
+     */
 }
