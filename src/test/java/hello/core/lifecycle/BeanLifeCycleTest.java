00ea4fcaec884dfd8c2770afe81157de3b1f3e5d
Jin Young Kim <wlsdud66523@kakao.com>
2026-01-12 02:46:22 +0900

    セクション９ ー アノテーション @PostConstruct, @PreDestroy

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -19,12 +19,9 @@ class BeanLifeCycleTest {
 
     @Configuration
     static class LifeCycleConfig {
-        @Bean(initMethod = "init", destroyMethod = "close") // Beanではなく設定ファイルで初期化・消滅メソッドを指定
-        NetworkClient networkClient() {                     // ・外部ライブラリのクラスであっても初期化・消滅メソッドスペックを確認して設定ファイルで指定すれば
-                                                            // 初期化・消滅メソッドを実行させることが出来るようになった
-                                                            // destroyMethodのデフォルトはdestroyMethod＝"(inferred)"(推論)である
-                                                            // 外部ライブラリの消滅メソッド名はほとんどclose(), shutdown()になっている。そこでSpringはdestroyMethodを指定しなくても、
-                                                            // 外部ライブラリにあるclose(),shutdown()を探して自動で呼び出す
+        @Bean
+        NetworkClient networkClient() {
+
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("https://hello-spring.dev");
             return networkClient;
--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,10 @@
 package hello.core.lifecycle;
 
+// jakarataパッケージ : JAVA標準のパッケージ
+// Spring Framework 6 / Spring Boot 3以降は"javax"使用は禁止されて、"jakarta"というパッケージ名を強制するようになった
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 class NetworkClient {
 
     private String url;
@@ -9,14 +14,21 @@ class NetworkClient {
     }
 
     /**
-     * ・これでSpraing Bean(NetworkClient)コードにSpring Containerへの依存性がなくなった
-     * ・これで初期化・消滅メソッド名を自由に変更できる
+     * @PostConstruct, @PreDestroy : Springで推奨する方法
+     * →どれも純粋JAVA標準アノテーション
+     * ・唯一の短所
+     * 外部ライブラリに適用不可能
+     * →外部ライブラリはそもそもコードの変更が不可能だったことを思い出そう。よってもしNetWorkClinetが外部ライブラリの場合@PostConstruct, @PreDestoryをコードに追加しなければならない
+     * が外部外部ライブラリであるため不可能。
+     * 方針としては@PostConstruct, @PreDestroyをベースとしてもし初期化・消滅メソッドを使わねばならない時は@Bean(initMethod, DestoryMethod)を使えばよい
      */
+    @PostConstruct
     public void init() {
         connect();
         call("初期化メッセージ");
     }
 
+    @PreDestroy
     public void close() {
         System.out.println("NetworkClient close()");
         disconnect();
